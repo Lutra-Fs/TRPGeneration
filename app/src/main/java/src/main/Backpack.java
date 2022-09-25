72a50eefdf312f923986e0649e948f8a8a36b239
Bo ZHANG <u7275896@anu.edu.au>
2022-09-25 18:42:47 +1000

    feat(backpack): add interact methods for Backpack Class

--- app/src/main/java/src/main/Backpack.java
+++ app/src/main/java/src/main/Backpack.java
@@ -4,19 +4,24 @@ import java.util.List;
 
 public class Backpack {
     List<Thing> things;
-    Thing sell(int index){
+
+    Thing sell(int index) {
         return things.remove(index);
     }
-    void buy(Thing t){
+
+    void buy(Thing t) {
         things.add(t);
     }
-    List<Thing> getThings(){
+
+    List<Thing> getThings() {
         return things;
     }
-    Thing sell(Thing t){
+
+    Thing sell(Thing t) {
         things.remove(t);
         return t;
     }
+
     Thing sell(String name) throws GameException {
         for (Thing t : things) {
             if (t.name.equals(name)) {
@@ -27,17 +32,36 @@ public class Backpack {
         throw new GameException("No such thing");
     }
 
+    public void add(Thing t) {
+        things.add(t);
+    }
 
+    void use(Thing t, FighterState f) throws GameException {
+        if (!things.remove(t)) {
+            throw new GameException("No such thing");
+        }
+        t.use(f);
+    }
 
-    class Thing{
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("Backpack: \n");
+        for (Thing t : things) {
+            s.append(t.toString()).append("\n");
+        }
+        return s.toString();
+    }
+
+    class Thing {
         final String name;
         final int price;
         final int recover;
-        void use(FighterState f){
+
+        void use(FighterState f) {
             f.addHP(recover);
         }
 
-        Thing(String name, int price, int recover){
+        Thing(String name, int price, int recover) {
             this.name = name;
             this.price = price;
             this.recover = recover;
