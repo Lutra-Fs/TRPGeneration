389484631e10e995edef33269b4cc13e87207419
Bo ZHANG <u7275896@anu.edu.au>
2022-09-25 17:55:14 +1000

    feat(NPC): add constructor for NPC class; mark name as final

--- app/src/main/java/src/main/NPC.java
+++ app/src/main/java/src/main/NPC.java
@@ -2,8 +2,17 @@ package src.main;
 
 public abstract class NPC {
     Location loc;
-    String name;
+    final String name;
+
+    public NPC(String name, int x, int y) {
+        this.name = name;
+        loc = new Location(x, y);
+    }
+
     void interact(Player p){
         p.p = Player.PlayerState.INTERACTING;
+    }
+    void interrupt(Player p){
+        p.p = Player.PlayerState.NORMAL;
     }
 }
