d77bce7b91bb54c4523186a7c5dc23237bbf8a22
Bo ZHANG <u7275896@anu.edu.au>
2022-09-25 16:00:00 +1000

    fix(game-exception): fix the grammar error of expection

---
+++ src/GameException.java
@@ -0,0 +1,7 @@
+package src;
+
+public class GameException extends Exception {
+    public GameException(String message) {
+        super(message);
+    }
+}
--- src/GameExpection.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class GameExpection extends Exception {
-    public GameExpection(String message) {
-        super(message);
-    }
-}
