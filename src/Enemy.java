4341597db0c7bffdf95f9289b824b73631e71d1a
Juhao Tao <u7342936@anu.edu.au>
2022-09-25 16:23:19 +0800

    Extend to NPC

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,13 +1,23 @@
 package src;
 
-public class Enemy {
+public class Enemy extends NPC {
 
     private int level;
     FighterStat state;
 
-    public Enemy(int level, FighterStat state) {
+    public Enemy(String name, int x, int y, int level, FighterStat state) {
+        super(name, x, y);
         this.level = level;
         this.state = state;
+    }
+
+    public Enemy(String name, int x, int y) {
+        super(name, x, y);
+    }
+
+    @Override
+    void interact(Player p) {
+        p.p = Player.PlayerState.FIGHTING;
     }
 
     public FighterStat getState() {
