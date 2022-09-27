1821c3b16f0b24c24eb5ff0a5d618569cd58a985
Bo ZHANG <u7275896@anu.edu.au>
2022-09-28 07:03:28 +1000

    refactor(enemy): Replace int with inner enum class
    The integer used for level has now been replaced by a inner enum class.
    It aims to increase the code's readability. The enum class also provide
    better error handling process. The code has been reformatted as well.

--- app/src/main/java/src/main/Enemy.java
+++ app/src/main/java/src/main/Enemy.java
@@ -2,19 +2,19 @@ package src.main;
 
 public class Enemy extends NPC {
 
-    private int level;
+    private final EnemyState level;
     FighterStat state;
 
     /**
-     * @param name NPC name
-     * @param x location x-coordinate
-     * @param y location y-coordinate
+     * @param name  NPC name
+     * @param x     location x-coordinate
+     * @param y     location y-coordinate
      * @param level NPC level
      * @param state NPC statement
      */
     public Enemy(String name, int x, int y, int level, FighterStat state) {
         super(name, x, y);
-        this.level = level;
+        this.level = EnemyState.getEnemyState(level);
         this.state = state;
     }
 
@@ -27,4 +27,21 @@ public class Enemy extends NPC {
         p.p = Player.PlayerState.FIGHTING;
     }
 
+    enum EnemyState {
+        NORMAL,
+        ELITE,
+        BOSS;
+
+        static EnemyState getEnemyState(int level) {
+            return switch (level) {
+                case 2 -> ELITE;
+                case 3 -> BOSS;
+                default -> NORMAL; // incl. level 1
+            };
+        }
+    }
+
+    EnemyState getLevel() {
+        return level;
+    }
 }
