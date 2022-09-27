19e546ae6b64349bc462d82bce17960cf7d29aa4
Juhao Tao <u7342936@anu.edu.au>
2022-09-27 18:09:29 +0800

    Add Java Docs

--- app/src/main/java/src/main/Enemy.java
+++ app/src/main/java/src/main/Enemy.java
@@ -5,19 +5,25 @@ public class Enemy extends NPC {
     private int level;
     FighterStat state;
 
+    /**
+     * @param name NPC name
+     * @param x location x-coordinate
+     * @param y location y-coordinate
+     * @param level NPC level
+     * @param state NPC statement
+     */
     public Enemy(String name, int x, int y, int level, FighterStat state) {
         super(name, x, y);
         this.level = level;
         this.state = state;
     }
 
+    /**
+     * @param p play state
+     */
     @Override
     void interact(Player p) {
         p.p = Player.PlayerState.FIGHTING;
-    }
-
-    public FighterStat getState() {
-        return state;
     }
 
 }
