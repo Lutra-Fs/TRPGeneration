edc39f4ef43227fb8067eacbd92b987ae1fc7b9d
Juhao Tao <u7342936@anu.edu.au>
2022-09-27 16:29:34 +0800

    Add Notes of each method

--- app/src/main/java/src/main/FighterStat.java
+++ app/src/main/java/src/main/FighterStat.java
@@ -25,99 +25,77 @@ public class FighterStat {
         this.MaxMP = MP;
         this.MaxHP = HP;
     }
+    //Set
     public List<Skill> getSkills() {
         return skills;
     }
     public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
-    public int getHP() {
-        return HP;
-    }
-
     public void setHP() throws GameException {
+        //If level up, the HP set to MaxHP
         levelUp();
         this.HP = getMaxHP();
     }
-
-    public void addHP(int HP)  {
-        if(HP+getHP()>MaxHP) {
-           this.HP = HP+getHP();
-        }
-    }
-
-    public void addMP(int MP)  {
-        if(MP+getMP()>MaxMP) {
-            this.MP = MP+getMP();
-        }
-    }
-
-    public int getMaxHP() {
-        return MaxHP;
-    }
-
     public void setMaxHP(int maxHP) {
         MaxHP = maxHP;
     }
-
-    public int getMaxMP() {
-        return MaxMP;
-    }
-
     public void setMaxMP(int maxMP) {
         MaxMP = maxMP;
     }
-
-    public int getMP() {
-        return MP;
-    }
-
     public void setMP() throws GameException {
+        //If level up, the MP set to MaxMP
         levelUp();
         this.MP = getMaxMP();
+    }
+    public void setAtk(int atk) {this.atk = atk;}
+    public void setDef(int def) {this.def = def;}
+    public void setExp(int exp) {this.exp = exp;}
+
+    //Get
+    public int getHP() {
+        return HP;
+    }
+    public int getMaxHP() {
+        return MaxHP;
+    }
+    public int getMaxMP() {
+        return MaxMP;
+    }
+    public int getMP() {
+        return MP;
     }
     public int getFighterLevel() {
         return fighterLevel;
     }
-    public void setFighterLevel(int fighterLevel) {
-        this.fighterLevel = fighterLevel;
-    }
-
-
-
-    public int getAtk() {
-        return atk;
-    }
-
-    public void setAtk(int atk) {
-        this.atk = atk;
-    }
-
-    public int getDef() {
-        return def;
-    }
-
-    public void setDef(int def) {
-        this.def = def;
-    }
-
-    public int getExp() {
-        return exp;
-    }
-
-    public void setExp(int exp) {
-        this.exp = exp;
-    }
-
+    public void setFighterLevel(int fighterLevel) {this.fighterLevel = fighterLevel;}
+    public int getAtk() {return atk;}
+    public int getDef() {return def;}
+    public int getExp() {return exp;}
     public Skill getSkill(int index){
         return skills.get(index);
     }
-
     public Skill getSkillAI(){
         //Default_baseAttack
         return getSkill(0);
     }
+    
+    //Other Function
+    public void addHP(int HP)  {
+        //HP cannot exceed maxHP
+        if(HP+getHP()>MaxHP) {
+            this.HP = HP+getHP();
+        }
+    }
+
+    public void addMP(int MP)  {
+         //MP cannot exceed maxMP
+        if(MP+getMP()>MaxMP) {
+            this.MP = MP+getMP();
+        }
+    }
     public Skill calcSkillATK(Skill s){
+        //Calculate skill damage according to fighter level
         int damage = s.getAtk();
         damage += getFighterLevel()*10;
         s.setAtk(damage);
@@ -125,18 +103,21 @@ public class FighterStat {
     }
 
     public void levelUp() throws GameException {
+        //if exp exceed each level exp, fighter level up
         if(getExp()<eachLevelExp){
             throw new GameException();
         }
     }
 
     public void beAttacked(Skill s) throws GameException {
+        // if attack damage is larger than defence, fighter received damage
         if(calcSkillATK(s).getAtk()<getDef()){
             throw new GameException();
         }
     }
 
     public boolean isDead(){
+        ///if HP is equal or lower than 0, fighter dead
         return getHP() <= 0;
     }
 
