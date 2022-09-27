14d253cb5fda803f1f51a5b0f9babd6f367fca0d
Juhao Tao <u7342936@anu.edu.au>
2022-09-27 18:07:57 +0800

    Add Java Docs

--- app/src/main/java/src/main/Skill.java
+++ app/src/main/java/src/main/Skill.java
@@ -4,23 +4,32 @@ public class Skill {
     String name;
     int atk;
 
+    /**
+     * @param name Skill name
+     * @param atk skill attack damage
+     */
     public Skill(String name, int atk) {
         this.name = name;
         this.atk = atk;
     }
 
+    /**
+     * @return skill name
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    /**
+     * @return skill damage
+     */
     public int getAtk() {
         return atk;
     }
 
+    /**
+     * @param atk set Skill attack with new damage
+     */
     public void setAtk(int atk) {
         this.atk = atk;
     }
