d17cb99ad7006ec0418315f39d4aacef7078238b
Bo ZHANG <u7275896@anu.edu.au>
2022-10-02 01:34:16 +1000

    fix(Level): add missing member 'gamePath'
    add missing member 'gamePath'

--- app/src/main/java/src/main/Level.java
+++ app/src/main/java/src/main/Level.java
@@ -1,100 +1,53 @@
 package src.main;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Level {
     // boolean in coordinates: true = wall, false = empty
-    int[][] coordinates;
+    boolean[][] coordinates;
     final int curLevel;
-    Map<Location, Boolean> canSave;
     Map<Location, NPC> npcs;
     Set<Location> npcLocation;
 
-    public Level(int curLevel) {
+    final Location startLoc;
+
+    final Location endLoc;
+
+    static String gamePath;
+
+    public Level(int curLevel, int maxX, int maxY, boolean[][] coordinates, Map<Location, NPC> npcs, Set<Location> npcLocation, Location startLoc, Location endLoc) {
         this.curLevel = curLevel;
-        this.coordinates = this.getCoordinates();
-        this.canSave = new HashMap<>();
-        this.npcs = new HashMap<>();
-        this.npcLocation = new HashSet<>();
-    }
-
-    // GETs
-    public int getCurLevel() {
-        return this.curLevel;
-    }
-
-    public int[][] getCoordinates() {
-        return this.coordinates;
-    }
-
-    public Map<Location, Boolean> getCanSave() {
-        return this.canSave;
-    }
-
-    public Map<Location, NPC> getNpcs() {
-        return this.npcs;
-    }
-
-    public Set<Location> getNpcLocation() {
-        return this.npcLocation;
-    }
-
-    // SETs
-    public void setNpcs(Map<Location, NPC> npcs) {
-        this.npcs = npcs;
-    }
-
-    public void setNpcLocation(Set<Location> npcLocation) {
-        this.npcLocation = npcLocation;
-    }
-
-    public void setCoordinates(int[][] coordinates) {
         this.coordinates = coordinates;
+        this.npcs = npcs;
+        this.npcLocation = npcLocation;
+        this.startLoc = startLoc;
+        this.endLoc = endLoc;
+        Location.setMax(maxX, maxY);
     }
 
-    public void setCanSave(Map<Location, Boolean> canSave) {
-        this.canSave = canSave;
+    boolean interactable(Location l) {
+        return npcLocation.contains(l);
     }
 
-    // ADDs
-    public void addNpc(Location location, NPC npc) {
-        this.npcs.put(location, npc);
+    NPC getNPC(Location l) {
+        return npcs.get(l);
     }
 
-    public void addNpcLocation(Location location) {
-        this.npcLocation.add(location);
+    boolean isWall(Location l) {
+        return coordinates[l.x][l.y];
     }
 
-    public void addCoordinates(int x, int y, int value) {
-        this.coordinates[x][y] = value;
+    void levelUp(Player p) throws IOException {
+        if (p.curLoc.equals(endLoc)) {
+            SaveParser.loadNextLevel(this);
+            p.curLoc = this.startLoc;
+        }
     }
 
-    public void addCanSave(Location location, Boolean canSave) {
-        this.canSave.put(location, canSave);
+    synchronized static void setGamePath(String path) {
+        gamePath = path;
     }
 
-    // REMOVEs
-    public void removeNpc(Location location) {
-        this.npcs.remove(location);
-    }
-
-    public void removeNpcLocation(Location location) {
-        this.npcLocation.remove(location);
-    }
-
-    public void removeCoordinates(int x, int y) {
-        this.coordinates[x][y] = 0;
-    }
-
-    public void removeCanSave(Location location) {
-        this.canSave.remove(location);
-    }
-
-    // OTHER
-    public void moveNpc(Location oldLocation, Location newLocation) {
-        NPC npc = this.npcs.get(oldLocation);
-        this.npcs.remove(oldLocation);
-        this.npcs.put(newLocation, npc);
-    }
 
 }
