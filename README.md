c3b60996fe3d9c886886d44abe1bbd44893d59e8
Bo ZHANG <u7275896@anu.edu.au>
2022-10-02 09:32:58 +1100

    feat(TUI): Add about dialog; introducing LICENSE
    add GPL-3.0 to LICENSE file and add instructions to it

---
+++ README.md
@@ -0,0 +1,26 @@
+# TRPGeration
+
+a simple RPG game engine.
+Copyright (C) 2022 Bo ZHANG; Jingqi DOU; Juhao TAO; Xiangda LI; Ge ZHAN
+
+## Installation
+
+See [Wikis](https://gitlab.cecs.anu.edu.au/u7275896/comp2120-2022-group-assignment-2/-/wikis/Installation-Instruction)
+for build instruction.
+
+Currently, jar file is not available.
+
+## Disclaimer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- app/src/main/java/src/main/App.java
+++ app/src/main/java/src/main/App.java
@@ -13,6 +13,8 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 public class App {
@@ -57,6 +59,28 @@ public class App {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+            }));
+            mainMenuPanel.addComponent(new Button("About", () -> {
+                new MessageDialogBuilder()
+                        .setTitle("About")
+                        .setText("TRPGeration\n" +
+                                "a simple RPG game engine.\n" +
+                                "Copyright (C) 2022  Bo ZHANG; Jingqi DOU; Juhao TAO; Xiangda LI; Ge ZHAN\n" +
+                                "This program is free software: you can redistribute it and/or modify\n" +
+                                "it under the terms of the GNU General Public License as published by\n" +
+                                "the Free Software Foundation, either version 3 of the License, or\n" +
+                                "(at your option) any later version.\n" +
+                                "\n" +
+                                "This program is distributed in the hope that it will be useful,\n" +
+                                "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+                                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+                                "GNU General Public License for more details.\n" +
+                                "\n" +
+                                "You should have received a copy of the GNU General Public License\n" +
+                                "along with this program.  If not, see <https://www.gnu.org/licenses/>.")
+                        .addButton(MessageDialogButton.Close)
+                        .build()
+                        .showDialog(gui);
             }));
 
 //------------------New Game Menu------------------//
